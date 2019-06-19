f54c3f9f3b8981e2972dd99422ba910418e7c9b0
nirav-tukadiya <nirav.tukadiya@gmail.com>
2019-06-19 17:08:34 +0530

    Project setup

---
+++ app/src/main/java/in/androidgeek/afe/ui/login/LoginResult.kt
@@ -0,0 +1,9 @@
+package `in`.androidgeek.afe.ui.login
+
+/**
+ * Authentication result : success (user details) or error message.
+ */
+data class LoginResult(
+    val success: LoggedInUserView? = null,
+    val error: Int? = null
+)
